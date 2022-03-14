c3312d582c078f54515ba692e1f4ed808dea8879
JinuCheon <woojin8787@gmail.com>
2022-03-14 22:17:43 +0900

    :tada: Backend initialize

---
+++ back/src/main/java/crappyUnivLife/timeMachineLetter/TimeMachineLetterApplication.java
@@ -0,0 +1,13 @@
+package crappyUnivLife.timeMachineLetter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimeMachineLetterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimeMachineLetterApplication.class, args);
+	}
+
+}
---
+++ back/src/test/java/crappyUnivLife/timeMachineLetter/TimeMachineLetterApplicationTests.java
@@ -0,0 +1,13 @@
+package crappyUnivLife.timeMachineLetter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimeMachineLetterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
