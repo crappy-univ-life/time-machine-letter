a6624872be5422b1134bd54d9fa6c0fd4678337c
Yoon-Hae-Min <yunhatmi@naver.com>
2022-03-17 23:43:45 +0900

    :lipstick: 편지 열람페이지 UI개발
    1. 편지지를 이미지로 활용하기 위해 이미지 추가
    2. 모바일 화면에서도 유지가 가능하게 이미지 사이즈 고정
    3. 결과창을 보여주므로 따로 close 버튼이 없음

--- front/src/index.js
+++ front/src/index.js
@@ -4,10 +4,11 @@ import App from './pages/App';
 import 'antd/dist/antd.css';
 import 'antd-button-color/dist/css/style.css';
 import Main from './pages/Main';
+import Letter from './components/Letter';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Main />
+    <Letter />
   </React.StrictMode>,
   document.getElementById('root'),
 );
