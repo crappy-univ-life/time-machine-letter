54c8ff4fb0331c2d3e528a8bccc58859458552c6
JinuCheon <woojin8787@gmail.com>
2022-04-07 07:39:46 +0900

    ✅ 편지 패스워드 암호화 테스트

---
+++ back/src/test/java/crappyUnivLife/timeMachineLetter/config/WebSecurityConfigTest.java
@@ -0,0 +1,29 @@
+package crappyUnivLife.timeMachineLetter.config;
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+class WebSecurityConfigTest {
+
+    @Autowired
+    PasswordEncoder passwordEncoder;
+
+    @Test
+    public void 비밀번호_암호화_테스트() {
+        String text = "1234";
+
+        String result = passwordEncoder.encode(text);
+        assertAll(
+                () -> assertNotEquals(result, text),
+                () -> assertTrue(passwordEncoder.matches("1234", result))
+        );
+    }
+}
