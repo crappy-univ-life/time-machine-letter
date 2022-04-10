1727b4bf5477bc2529c43b3c63f5d8e31e0a8b21
Yoon-Hae-Min <yunhatmi@naver.com>
2022-04-10 13:49:01 +0900

    :art: 메인화면 에서 로그인페이지로 이동 구조 수정
    1. router 기능을 이용해 정보가 없을시 로그인 페이지로 이동 구현

--- front/src/pages/Routers.js
+++ front/src/pages/Routers.js
@@ -2,14 +2,21 @@ import { Routes, Route, Link } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import Login from './Login';
 import KakaoHandler from './KakaoHandler';
-import Main from './Main';
+import Main, { RequireAuth } from './Main';
 import Letter from './Letter';
 
 function Routers() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={<Main />} />
+        <Route
+          path="/"
+          element={(
+            <RequireAuth>
+              <Main />
+            </RequireAuth>
+          )}
+        />
         <Route path="/login" element={<Login />} />
         <Route path="/kakao_callback" element={<KakaoHandler />} />
         <Route path="/letter/:hash" element={<Letter />} />
