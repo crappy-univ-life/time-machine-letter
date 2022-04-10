bd2888b8571aaa337cc66fe3f8a29f4b7a0cc13d
Yoon-Hae-Min <yunhatmi@naver.com>
2022-04-10 19:31:44 +0900

    :lipstick: 편지 리스트 제목과 날짜 위치 및 format 조정

--- front/src/components/LetterList.js
+++ front/src/components/LetterList.js
@@ -1,6 +1,7 @@
 import { Card, Col, Row } from 'antd';
 import Meta from 'antd/lib/card/Meta';
 import { useDispatch, useSelector } from 'react-redux';
+import moment from 'moment';
 import style from '../css/Main.module.css';
 import useModal from '../Hooks/useModal';
 import { openDetailModal } from '../store/global';
@@ -22,7 +23,7 @@ function LetterList({ openLetter, closeLetter, allLetter }) {
     <Row gutter={[20, 20]} align="center" sm={12}>
       {showList.map((letter) => (
         <Col lg={12} xs={22}>
-          <Card bordered={false} hoverable className={style.card} onClick={() => dispatch(openDetailModal(letter.hash))}><Meta title={letter.createAt} description={letter.title} /></Card>
+          <Card bordered={false} hoverable className={style.card} onClick={() => dispatch(openDetailModal(letter.hash))}><Meta title={letter.title} description={moment(letter.createAt).format('YYYY-MM-DD')} /></Card>
         </Col>
       ))}
     </Row>
