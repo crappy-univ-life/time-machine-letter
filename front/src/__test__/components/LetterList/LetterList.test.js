815e8573ecb49a48a8d9361c17bfb24f9f0858d3
Yoon-Hae-Min <yunhatmi@naver.com>
2022-06-02 00:09:01 +0900

    :white_check_mark: LetterList 테스트코드 동작 로직 변경

--- front/src/__test__/components/LetterList/LetterList.test.js
+++ front/src/__test__/components/LetterList/LetterList.test.js
@@ -1,28 +1,22 @@
-import { useDispatch, useSelector } from 'react-redux';
 import { render, screen, fireEvent } from '../../../../test.utils';
 import LetterList from '../../../components/LetterList/LetterList';
 import { dummyLetter } from '../../../fixtures/dummyLetter';
 
-jest.mock('react-redux');
-
 describe('LetterList 테스트코드', () => {
   beforeEach(() => {
-    const dispatch = jest.fn();
-    useDispatch.mockImplementation(() => dispatch);
-    useSelector.mockImplementation((selector) => selector(dummyLetter));
     jest.useFakeTimers().setSystemTime(new Date('2022-05-20T06:10:16.000+00:00'));
   });
 
-  it('열람가능한 편지', () => {
-    render((<LetterList openLetter />));
-    expect(screen.getByText('오픈된 편지')).toBeInTheDocument();
+  it('열람가능한 편지', async () => {
+    render(<LetterList openLetter />, { initialState: dummyLetter });
+    expect(await screen.findByText('오픈된 편지')).toBeInTheDocument();
   });
   it('열람불가능한 편지', () => {
-    render((<LetterList closeLetter />));
+    render((<LetterList closeLetter />), { initialState: dummyLetter });
     expect(screen.getByText('오픈되지 않은 편지')).toBeInTheDocument();
   });
   it('모든편지', () => {
-    render((<LetterList allLetter />));
+    render((<LetterList allLetter />), { initialState: dummyLetter });
     expect(screen.getByText('오픈된 편지')).toBeInTheDocument();
     expect(screen.getByText('오픈되지 않은 편지')).toBeInTheDocument();
   });
--- front/test.utils.js
+++ front/test.utils.js
@@ -1,16 +1,37 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
+import { Provider } from 'react-redux';
+import { configureStore } from '@reduxjs/toolkit';
+import store from './src/store';
+import { LoginApi } from './src/service/login';
+import { LetterApi } from './src/service/Letter';
+import auth from './src/store/auth';
+import letter from './src/store/letter';
+import global from './src/store/global';
 
-function AllTheProviders({ children }) {
-  return (
-    <div>
-      {children}
-    </div>
-  );
-}
-
-const customRender = (ui, options) => render(ui, { wrapper: AllTheProviders, ...options });
+const customRender = (ui, {
+  initialState = {},
+  customStore = configureStore({
+    preloadedState: initialState,
+    reducer: {
+      [LoginApi.reducerPath]: LoginApi.reducer,
+      [LetterApi.reducerPath]: LetterApi.reducer,
+      auth,
+      letter,
+      global,
+    },
+  }),
+} = {}, options) => {
+  function AllTheProviders({ children }) {
+    return (
+      <Provider store={customStore}>
+        {children}
+      </Provider>
+    );
+  }
+  return render(ui, { wrapper: AllTheProviders, ...options });
+};
 
 // re-export everything
 export * from '@testing-library/react';
