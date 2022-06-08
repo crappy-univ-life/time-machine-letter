3fef628edb4f2183df2d671b206d611c2b2e78ba
Seungbin Bae <deanidh1011@gmail.com>
2022-06-08 19:40:42 +0900

    ✨카카오 로그인 구현 (#67)
    Model 폴더에 KakaoLoginManager 추가
    로그인 성공시 페이지 변경, 실패시 알림창 팝업

--- iOS/TimeMachineLetter/View/LoginViewController.swift
+++ iOS/TimeMachineLetter/View/LoginViewController.swift
@@ -6,8 +6,13 @@
 //
 
 import UIKit
+import Alamofire
+import KakaoSDKAuth
+import KakaoSDKUser
 
 class LoginViewController: UIViewController {
+
+    var kakaoLoginManager = KakaoLoginManager()
     
     var titleLabel: UILabel!
     var loginButton: UIButton!
@@ -19,6 +24,7 @@ class LoginViewController: UIViewController {
         
         titleLabel = UILabel()
         titleLabel.text = "Timemachine\nLetter"
+        titleLabel.textColor = .black
         titleLabel.font = UIFont.systemFont(ofSize: 40)
         titleLabel.textAlignment = .center
         titleLabel.numberOfLines = 2
@@ -55,15 +61,40 @@ class LoginViewController: UIViewController {
             descLabel.bottomAnchor.constraint(equalTo: view.layoutMarginsGuide.bottomAnchor, constant: -20),
         ])
     }
-    
-    @objc func buttonAction(sender: UIButton!) {
-                
-        let mainVC = MainViewController()
-        mainVC.modalPresentationStyle = .fullScreen
-        present(mainVC, animated: true)
-    }
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        kakaoLoginManager.delegate = self
+    }
+    
+    @objc func buttonAction(sender: UIButton!) {
+       
+        if UserApi.isKakaoTalkLoginAvailable() {
+            UserApi.shared.loginWithKakaoTalk {(oauthToken, error) in
+                self.kakaoLoginManager.loginWithToken(oauthToken: oauthToken, error: error)
+            }
+        } else {
+            UserApi.shared.loginWithKakaoAccount {(oauthToken, error) in
+                self.kakaoLoginManager.loginWithToken(oauthToken: oauthToken, error: error)
+            }
+        }
+    }
+}
+
+// MARK: - KaKaoLoginManagerDelegate
+
+extension LoginViewController: KaKaoLoginManagerDelegate {
+    
+    func didLoginSuccess() {
+        let mainVC = MainViewController()
+        mainVC.modalPresentationStyle = .fullScreen
+        self.present(mainVC, animated: true)
+    }
+    
+    func didFailWithError(error: Error) {
+        let alert = UIAlertController(title: "로그인 실패", message: "\(error.localizedDescription)", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "확인", style: .cancel))
+        self.present(alert, animated: true)
     }
 }
