0f70d045b5abfc41b0e5d0b09516f5d987b514a9
MinwuKim <mwk300@nyu.edu>
2022-06-11 15:47:56 +0900

    :sparkles:백엔드 리팩토링을 위한 환경 구축

---
+++ backend-refactoring/src/test/java/minwu/minwutml/MinwuTmlApplicationTests.java
@@ -0,0 +1,13 @@
+package minwu.minwutml;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinwuTmlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
