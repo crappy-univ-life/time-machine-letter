0c2e32d15dcc31cc303c5ca4f7ebe5921936b5f5
Yoon-Hae-Min <yunhatmi@naver.com>
2022-06-16 16:48:57 +0900

    :white_check_mark: API 네이밍 변경에 따른 dummyData 이름 변경

--- front/src/__mocks__/Letter.js
+++ front/src/__mocks__/Letter.js
@@ -1,6 +1,7 @@
 import { rest } from 'msw';
 
 const date = new Date('2022-04-04T23:50:00');
+const baseURL = 'http://timemachineletter.tk:8080/';
 const dummy = {
   readable: true,
   id: 3,
@@ -8,9 +9,10 @@ const dummy = {
   openAt: date,
   title: '제목1',
   content: '첫번째 테스트 본문입니다',
-  from: '개발자',
-  to: '뭐',
+  letterFrom: '개발자',
+  letterTo: '뭐',
 };
 export const Letter = [
-  rest.get('http://timemachineletter.tk:8080/letter/:hash', (req, res, ctx) => res(ctx.status(200), ctx.json(dummy))),
+  rest.get(`${baseURL}letter/:hash`, (req, res, ctx) => res(ctx.status(200), ctx.json(dummy))),
+  rest.delete(`${baseURL}letter/:hash`, (req, res, ctx) => res(ctx.status(200))),
 ];
